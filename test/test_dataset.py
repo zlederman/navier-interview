69363f66566aecf2231ddcb5ec4487a91a1022bc
Zachary Lederman <zlederman00@gmail.com>
2024-11-29 15:08:25 -0500

    looking at torch dataset

--- test/test_dataset.py
+++ test/test_dataset.py
@@ -1,2 +1,13 @@
+from src.process.airfrans import process_airfrans
+from src.training.dataset import NACAPositionVelocityDataset
+from pathlib import Path
+import os
 def test_pytorch_dataset():
-    raise NotImplementedError
+    path = Path("test/data/")
+    h5_loc = Path("test/data/data.h5")
+    process_airfrans(path, h5_loc)
+    dataset = NACAPositionVelocityDataset(h5_loc, "data")
+    train, test = dataset[0]
+    assert train != test
+    
+    
